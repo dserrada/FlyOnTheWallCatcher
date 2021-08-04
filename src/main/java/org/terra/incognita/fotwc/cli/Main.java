0df606797343830898d0a999beffac98172da27e
David Pérez Serrada <dserrada@gmail.com>
2021-08-05 00:17:43 +0200

    Setting JDK to 16

---
+++ src/main/java/org/terra/incognita/fotwc/cli/Main.java
@@ -0,0 +1,13 @@
+package org.terra.incognita.fotwc.cli;
+
+/**
+ * Command line launcher
+ *
+ * @author dserrada@gmail.com
+ */
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Hello world");
+    }
+}
