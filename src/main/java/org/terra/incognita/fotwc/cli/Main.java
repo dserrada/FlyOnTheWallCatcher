2818bc34cdf0736fb863e647ac5b01efb9ef48a2
David Pérez Serrada <dserrada@gmail.com>
2021-08-08 00:47:39 +0200

    Logging with log4j2

--- src/main/java/org/terra/incognita/fotwc/cli/Main.java
+++ src/main/java/org/terra/incognita/fotwc/cli/Main.java
@@ -1,5 +1,13 @@
 package org.terra.incognita.fotwc.cli;
 
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+
+
 /**
  * Command line launcher
  *
@@ -7,7 +15,18 @@ package org.terra.incognita.fotwc.cli;
  */
 public class Main {
 
-    public static void main(String... args) {
-        System.out.println("Hello world");
+    /**
+     * Simple log4j2 logger
+     */
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    public static void main(final String... args) {
+        log.debug("Init logging system");
+        // Only a little test with log4j2 and lambda expression
+        log.trace("Running program in directory {} ", () ->  new File("").getAbsolutePath());
+        log.debug("Shutting down");
     }
+
+
+
 }
