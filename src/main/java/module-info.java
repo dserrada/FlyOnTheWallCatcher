48e2af5771918f8e80c5f09a1ea148e99f6b9557
David Pérez Serrada <dserrada@gmail.com>
2021-08-12 01:26:43 +0200

    Modularization

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module FlyOnTheWallCatcher.main {
-    // Based on https://www.baeldung.com/java-9-modularity
+    // Based on https://www.baeldung.com/java-9-modularity and http://tutorials.jenkov.com/java/modules.html#automatic-modules
     requires transitive jdk.net;
-    requires transitive org.apache.logging.log4j;
-    requires transitive org.apache.logging.log4j.core;
     requires transitive info.picocli;
+    requires org.apache.logging.log4j;
+
     exports org.terra.incognita.fotwc.cli;
+    exports org.terra.incognita.fotwc.api;
+
+    // Automatic modules
+    requires static org.apache.logging.log4j.core;
+
 }
